d58de0dfe3739e4013c75c44e41e7b3f3395400e
1003750 <1003750@rgu.ac.uk>
2016-03-08 09:54:09 +0000

    php try

--- w6-php/act6.php
+++ w6-php/act6.php
@@ -2,7 +2,9 @@
 
 $nameList = array(aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo);
 rsort($nameList);
-$winSpecs = rand($nameList);
+$winningnumber = rand(1, 15);
+echo $winningnumber;
+$winSpecs = $nameList($winningnumber);
 strtoupper($winSpecs);
 echo $winSpecs;
 echo "test";
