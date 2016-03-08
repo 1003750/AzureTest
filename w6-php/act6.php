bf03c17518578a40accab374f460e4d6c5b4ee44
1003750 <1003750@rgu.ac.uk>
2016-03-08 09:50:48 +0000

    php try

---
+++ w6-php/act6.php
@@ -0,0 +1,9 @@
+<?php
+
+$nameList = array(aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo);
+rsort($nameList);
+$winSpecs = rand($nameList);
+strtoupper($winSpecs);
+echo $winSpecs;
+
+?>
