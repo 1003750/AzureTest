c7b5486f3d1017c733e617d7ba973fe2c9c256c2
1003750 <1003750@rgu.ac.uk>
2016-03-08 10:42:37 +0000

    php try

--- w6-php/act6.php
+++ w6-php/act6.php
@@ -4,4 +4,6 @@ $nameList = array(aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo);
 rsort($nameList);
 $winningnumber = rand(0, 14);
 print "winner of all the specs is " . strtoupper($nameList[$winningnumber]);
+unset $nameList[$winnernumber];
+printmyArray($nameList);
 ?>
