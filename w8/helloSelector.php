a34e0303c91d3c042811807181cd0ceeb4d0ff1a
1003750 <1003750@rgu.ac.uk>
2016-03-21 10:40:07 +0000

    php try

--- w8/helloSelector.php
+++ w8/helloSelector.php
@@ -9,7 +9,7 @@
     <h>Where would you like to say hello?</h>
 
     <h3></h3><a href="http://654.azurewebsites.net/w8/helloPrinter.php?planet=Earth">Earth</a></h3>
-    <hr><a href="http://654.azurewebsites.net/w8/helloPrinter.php?planet=mars">Mars</a></h3>
-    <h3></h3><a href="http://654.azurewebsites.net/w8/helloPrinter.php?planet=uranus">Uranus</a></h3>
+    <hr><a href="http://654.azurewebsites.net/w8/helloPrinter.php?planet=Mars">Mars</a></h3>
+    <h3></h3><a href="http://654.azurewebsites.net/w8/helloPrinter.php?planet=Uranus">Uranus</a></h3>
 
 </body>
