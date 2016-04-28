5d44e3d8159bb73539e0202643c5cd78fe979af9
1003750 <1003750@rgu.ac.uk>
2016-04-28 15:27:43 +0100

    php try

--- w11/showbugs.php
+++ w11/showbugs.php
@@ -44,8 +44,8 @@
             $bugName = $row['bugName'];
             $BugCategory = $row['BugCategory'];
             $BugSummary = $row['BugSummary'];
-            echo "<h3><b>{$bugName}</b></h3>";
-            echo "<h3><i>{$BugCategory}</i></h3>";
+            echo "<h3>{$bugName}</h3>";
+            echo "<h4>{$BugCategory}</h4>";
             echo "<p>{$BugSummary}</p>";
             echo "<hr>";
         }
